ca34b069a9fefa76d17b9e172646b3cde2ef5248
ebraronder34-gif <ebraronder.34@gmail.com>
2026-09-17 16:41:40 +0200

    Add OTIF Performance Validation

--- sql/02_supplier_delivery_validation.sql
+++ sql/02_supplier_delivery_validation.sql
@@ -34,3 +34,57 @@ SELECT
     ) AS in_full_pct
 
 FROM purchase_orders;
+-- 3. Overall OTIF Performance Validation
+
+WITH parsed_dates AS (
+    SELECT
+        po_id,
+        ordered_qty,
+        received_qty,
+
+        printf(
+            '%04d-%02d-%02d',
+            CAST(substr(promised_date, -4) AS INTEGER),
+            CAST(substr(promised_date, 1, instr(promised_date, '/') - 1) AS INTEGER),
+            CAST(substr(
+                promised_date,
+                instr(promised_date, '/') + 1,
+                instr(substr(promised_date, instr(promised_date, '/') + 1), '/') - 1
+            ) AS INTEGER)
+        ) AS promised_date_parsed,
+
+        printf(
+            '%04d-%02d-%02d',
+            CAST(substr(receipt_date, -4) AS INTEGER),
+            CAST(substr(receipt_date, 1, instr(receipt_date, '/') - 1) AS INTEGER),
+            CAST(substr(
+                receipt_date,
+                instr(receipt_date, '/') + 1,
+                instr(substr(receipt_date, instr(receipt_date, '/') + 1), '/') - 1
+            ) AS INTEGER)
+        ) AS receipt_date_parsed
+
+    FROM purchase_orders
+)
+
+SELECT
+    COUNT(DISTINCT po_id) AS total_po,
+
+    COUNT(DISTINCT CASE
+        WHEN receipt_date_parsed <= promised_date_parsed
+         AND received_qty >= ordered_qty
+        THEN po_id
+    END) AS otif_po,
+
+    ROUND(
+        100.0 *
+        COUNT(DISTINCT CASE
+            WHEN receipt_date_parsed <= promised_date_parsed
+             AND received_qty >= ordered_qty
+            THEN po_id
+        END)
+        / COUNT(DISTINCT po_id),
+        1
+    ) AS otif_pct
+
+FROM parsed_dates;
